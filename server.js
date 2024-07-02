08e1a973ae30ad6971a6139e7d232c048954ca89
tanner-codes <tanner.codes@outlook.com>
2024-07-02 16:23:02 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,134 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-const path = require('path');
-const fs = require('fs');
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+// Game state
+const games = {};
+const publicGames = [];
 
-// In-memory data store for chat rooms and user profiles
-const chatRooms = {};
-const userProfiles = {};
+// Serve static files
+app.use(express.static('public'));
 
 // Socket.IO event handlers
 io.on('connection', (socket) => {
-  // Handle new user connection
-  socket.on('new-user', (username, profilePic) => {
-    userProfiles[socket.id] = { username, profilePic };
-    io.emit('user-connected', socket.id, username, profilePic);
+  console.log('A user connected');
+
+  // Get the player's username and profile picture
+  socket.on('setUsername', (username, pfp) => {
+    socket.username = username;
+    socket.pfp = pfp;
   });
 
-  // Handle chat room creation
-  socket.on('create-room', (roomName) => {
-    chatRooms[roomName] = [];
-    socket.join(roomName);
-    io.emit('room-created', roomName);
+  // Create a new game
+  socket.on('createGame', (isPublic) => {
+    const gameCode = generateGameCode();
+    const game = {
+      code: gameCode,
+      players: [{ id: socket.id, username: socket.username, pfp: socket.pfp }],
+      sentence: '',
+      currentPlayerIndex: 0,
+      isPublic: isPublic,
+    };
+    games[gameCode] = game;
+    socket.join(gameCode);
+    if (isPublic) {
+      publicGames.push(game);
+    }
+    socket.emit('gameCreated', gameCode, isPublic);
   });
 
-  // Handle joining a chat room
-  socket.on('join-room', (roomName) => {
-    socket.join(roomName);
-    io.to(roomName).emit('user-joined', socket.id, userProfiles[socket.id]);
-  });
-
-  // Handle new message
-  socket.on('new-message', (roomName, message, imageData) => {
-    const sender = userProfiles[socket.id];
-    const messageData = { sender, message, imageData };
-    chatRooms[roomName].push(messageData);
-    io.to(roomName).emit('message-received', messageData);
-  });
-
-  // Handle user disconnection
-  socket.on('disconnect', () => {
-    const user = userProfiles[socket.id];
-    if (user) {
-      io.emit('user-disconnected', socket.id, user.username);
-      delete userProfiles[socket.id];
+  // Join an existing game
+  socket.on('joinGame', (gameCode) => {
+    const game = games[gameCode];
+    if (game && game.players.length < 10) {
+      game.players.push({ id: socket.id, username: socket.username, pfp: socket.pfp });
+      socket.join(gameCode);
+      io.to(gameCode).emit('playerJoined', socket.id, socket.username, socket.pfp);
+      io.to(socket.id).emit('gameJoined', gameCode, game.sentence, game.players[game.currentPlayerIndex].id);
+    } else if (!game) {
+      socket.emit('gameNotFound');
+    } else {
+      socket.emit('gameIsFull');
     }
   });
+
+  // Join a random public game
+  socket.on('joinRandomPublicGame', () => {
+    const availablePublicGames = publicGames.filter(game => game.players.length < 10);
+    if (availablePublicGames.length > 0) {
+      const randomGame = availablePublicGames[Math.floor(Math.random() * availablePublicGames.length)];
+      const gameCode = randomGame.code;
+      randomGame.players.push({ id: socket.id, username: socket.username, pfp: socket.pfp });
+      socket.join(gameCode);
+      io.to(gameCode).emit('playerJoined', socket.id, socket.username, socket.pfp);
+      io.to(socket.id).emit('gameJoined', gameCode, randomGame.sentence, randomGame.players[randomGame.currentPlayerIndex].id);
+    } else {
+      socket.emit('noPublicGamesAvailable');
+    }
+  });
+
+  // Submit a word
+  socket.on('submitWord', (gameCode, word) => {
+    const game = games[gameCode];
+    if (game && game.players[game.currentPlayerIndex].id === socket.id) {
+      game.sentence += `${word} `;
+      if (word.endsWith('.') || word.endsWith('?') || word.endsWith('!')) {
+        io.to(gameCode).emit('gameEnded', game.sentence);
+        delete games[gameCode];
+        const publicGameIndex = publicGames.findIndex(g => g.code === gameCode);
+        if (publicGameIndex !== -1) {
+          publicGames.splice(publicGameIndex, 1);
+        }
+      } else {
+        game.currentPlayerIndex = (game.currentPlayerIndex + 1) % game.players.length;
+        io.to(gameCode).emit('sentenceUpdated', game.sentence, game.players[game.currentPlayerIndex].id);
+      }
+    }
+  });
+
+  // Send a chat message
+  socket.on('chatMessage', (gameCode, message) => {
+    const game = games[gameCode];
+    if (game) {
+      io.to(gameCode).emit('newMessage', socket.id, socket.username, message, socket.pfp);
+    }
+  });
+
+  // Handle disconnections
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+    Object.keys(games).forEach((gameCode) => {
+      const game = games[gameCode];
+      const playerIndex = game.players.findIndex((player) => player.id === socket.id);
+      if (playerIndex !== -1) {
+        const playerUsername = game.players[playerIndex].username;
+        const playerPFP = game.players[playerIndex].pfp;
+        game.players.splice(playerIndex, 1);
+        if (game.players.length === 0) {
+          delete games[gameCode];
+          const publicGameIndex = publicGames.findIndex(g => g.code === gameCode);
+          if (publicGameIndex !== -1) {
+            publicGames.splice(publicGameIndex, 1);
+          }
+        } else if (playerIndex === game.currentPlayerIndex) {
+          game.currentPlayerIndex = (game.currentPlayerIndex + 1) % game.players.length;
+        }
+        io.to(gameCode).emit('playerLeft', socket.id, playerUsername, playerPFP);
+      }
+    });
+  });
 });
+
+// Generate a random game code
+function generateGameCode() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let code = '';
+  for (let i = 0; i < 6; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+}
 
 // Start the server
 const PORT = process.env.PORT || 3000;
